4654c3e2f42cf6bdc6a08310b6038c3e44d44c3b
Nitesh sharma <niteshsharma@Niteshs-MacBook-Pro.local>
2018-03-16 12:31:26 +0530

    es6 new syntax

--- es6.js
+++ es6.js
@@ -1,36 +1,103 @@
-//let/constant 
-let name = "niyes "
+'use strict';
+
+//Let, const and block.
+let proudctId = 1;
+const MAX = 50;
 {
-	name  = "some"
+	let proudctId = 60;
 }
+console.log(proudctId, MAX)
 
-const MA = 10;
+//arrow function, this refers to context.
+let getPrice = (count) => count*2;
+console.log(getPrice(3))
 
-console.log(name);
+//setting default to parameters.
+let getPriceDefault = (count = 30) => count*2;
+console.log(getPriceDefault());
 
-//fat arrow
-let getPrice = () => 5.99;
-let getPrice2 = count => count*2;
-let getPrice3 = count => {
-	let v1 = 10;
-	return v1;
+//rest and spread parameter.
+let showCategories = (proudctId, ...categories)=> {
+	return categories;
 }
-console.log(getPrice());
-console.log(getPrice2(2));
+console.log(showCategories(1, "clothes", "shoes"));
 
-//default.
-var init = function(price = 20){
-	console.log(price)
+let price = [12,4,5];
+console.log(Math.max(...price))
+
+
+//object literal extension.
+let method = "prop";
+let productView = {
+	[method + '--001'] (){
+		console.log(method);
+	}
 }
-init();
+productView['prop--001']();
+
+//for of loop
+var count = 0;
+for (let code of [1,2,4]){
+	count++;
+}
+console.log(count);
+
+//template literal
+let invoice1 = '1243';
+console.log(`invoice : ${invoice1}`);
+
+//destructuring.
+let salary = [32, 4, 40, 5];
+let [low, high] = salary;
+
+console.log(low);
 
 
 
-//spread
-let foo =  (...param) => param;
-console.log(foo ("one", "two"))
 
-let price = [12, 20, 14];
-console.log(Math.max(...price));
 
-console.log(..."1,2,3")
+
+// //let/constant 
+// let name = "niyes "
+// {
+// 	name  = "some"
+// }
+
+// const MA = 10;
+
+// console.log(name);
+
+// //fat arrow
+// let getPrice = () => 5.99;
+// let getPrice2 = count => count*2;
+// let getPrice3 = count => {
+// 	let v1 = 10;
+// 	return v1;
+// }
+// console.log(getPrice());
+// console.log(getPrice2(2));
+
+// //default.
+// var init = function(price = 20){
+// 	console.log(price)
+// }
+// init();
+
+
+
+// //spread
+// let foo =  (...param) => param;
+// console.log(foo ("one", "two"))
+
+// let price = [12, 20, 14];
+// console.log(Math.max(...price));
+
+// console.log(..."1,2,3")
+
+// name = "jei"
+// consol e.log({
+// 	name : "some"
+// })
+
+// [low , avg] = [10000, 20 ];
+// console.log(low);
